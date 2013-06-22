1f6474d902784a6a4b279eaf8fea87b6d5b5c298
Lars Holdgaard <mcoroklo@gmail.com>
2013-06-23 00:08:30 +0200

    bitcoin web test project added

---
+++ ClientTest/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BitcoinWrapper.Wrapper;
+
+namespace ClientTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BaseBtcConnector _baseBtcConnector;
+        //
+        // GET: /Home/
+
+        public HomeController()
+        {
+            this._baseBtcConnector =  new BaseBtcConnector();;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public float GetBalanceInWallet()
+        {
+            var service = _baseBtcConnector.GetBalance();
+            return service;
+        }
+    }
+}
